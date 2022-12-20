6829fc8ca71ed3d84dc7e8461ed0a3089f77d66d
Daviti Didbaridze <ddidbaridze@gmail.com>
2022-12-20 14:34:35 +0100

    add optional chaining

--- src/index.ts
+++ src/index.ts
@@ -83,3 +83,20 @@ function greet(name: string | null | undefined) {
 
 greet(null)
 greet(undefined)
+//Optional Chaining
+type Customer = {
+    birthday?: Date
+}
+function getCustomer(id: number): Customer | null | undefined {
+    return id === 0 ? null : { birthday: new Date() }
+}
+
+let customer = getCustomer(0)
+//Optional property access operator
+console.log(customer?.birthday?.getFullYear())
+//Optional element access operator
+// customer?.[0]
+
+//Optional call
+let log: any = null
+log?.('a')
