8d8f25f9293898023c4a88335a17495cb46e4b1a
Daviti Didbaridze <ddidbaridze@gmail.com>
2022-12-20 13:32:53 +0100

    add enums

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,15 @@ let numbers: number[] = [1, 2, 3]
 let numbersAny = []
 // Tuples
 let user: [number, string] = [1, 'David']
+// Enums
+enum Size {
+    Small = 's',
+    Medium = 'm',
+    Large = 'l'
+}
+const enum Sizes {
+    Small = 1,
+    Medium,
+    Large
+}
+let mySize: Sizes = Sizes.Medium
