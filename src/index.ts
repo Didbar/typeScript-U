1c42448bfea4df22ed76f896164f347703295edb
Daviti Didbaridze <ddidbaridze@gmail.com>
2022-12-20 14:19:25 +0100

    add type intersection

--- src/index.ts
+++ src/index.ts
@@ -58,3 +58,16 @@ function kgToLbs(weight: number | string): number {
 
 kgToLbs(10)
 kgToLbs('10kg')
+//Intersection Types
+type Draggable = {
+    drag: () => void
+}
+type Resizable = {
+    resize: () => void
+}
+type UIWidget = Draggable & Resizable
+
+let textBox: UIWidget = {
+    drag: () => {},
+    resize: () => {}
+}
