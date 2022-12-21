a1d461f2d7f24f7ad3793b815fad0a48a6ed4fc3
Daviti Didbaridze <ddidbaridze@gmail.com>
2022-12-21 18:21:53 +0100

    add static properties

--- src/classes-interfaces-oop/index.ts
+++ src/classes-interfaces-oop/index.ts
@@ -16,16 +16,39 @@ class Account {
 }
 
 class SeatAssignment {
+    //NOTE: index signatures
     [seatNumber: string]: string
 }
+class Ride {
+    private static _activeRides: number = 0
 
+    start() {
+        Ride._activeRides++
+    }
+    stop() {
+        Ride._activeRides--
+    }
+
+    static get activeRides(): number {
+        return Ride._activeRides
+    }
+}
+//General Class
 let account = new Account(1, 'David', 0)
 account.deposit(10)
 console.log(account instanceof Account)
 console.log(account.balance)
 
+//Index signatures
 let seats = new SeatAssignment()
 seats.A1 = 'Kolia'
 seats.A2 = 'David'
 seats['Something fancy'] = 'some text'
 console.log(seats)
+
+//Static properties
+let ride1 = new Ride()
+ride1.start()
+let ride2 = new Ride()
+ride2.start()
+console.log(Ride.activeRides)
