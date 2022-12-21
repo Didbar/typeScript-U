f6c8f8bf5f372064979aaef5e18e3043863d4a9f
Daviti Didbaridze <ddidbaridze@gmail.com>
2022-12-21 19:11:48 +0100

    add abstract class

--- src/classes-interfaces-oop/index.ts
+++ src/classes-interfaces-oop/index.ts
@@ -43,7 +43,6 @@ class Person {
         console.log('walking')
     }
 }
-
 class Student extends Person {
     constructor(public studentId: number, firstName: string, lastName: string) {
         super(firstName, lastName)
@@ -53,7 +52,6 @@ class Student extends Person {
         console.log('Taking a Test')
     }
 }
-
 class Teacher extends Person {
     override get fullName() {
         return 'Professor ' + super.fullName
@@ -62,6 +60,20 @@ class Teacher extends Person {
 class Principal extends Person {
     override get fullName() {
         return 'Principal ' + super.fullName
+    }
+}
+//NOTE Abstract classes
+abstract class Shape {
+    constructor(public color: string) {}
+
+    abstract render(): void
+}
+class Circle extends Shape {
+    constructor(public radius: number, color: string) {
+        super(color)
+    }
+    override render(): void {
+        console.log('Rendering a circle')
     }
 }
 //NOTE Polymorphism
@@ -73,6 +85,9 @@ printNames([
     new Teacher('Cornelia', 'Joliet'),
     new Principal('Julius', 'Cosie')
 ])
+function printNames(people: Person[]) {
+    for (let person of people) console.log(person.fullName)
+}
 /*Prints
     - John Smith
     - Professor Lena Snow
@@ -82,35 +97,30 @@ printNames([
     - Principal Julius Cosie
 */
 
-function printNames(people: Person[]) {
-    for (let person of people) console.log(person.fullName)
-}
-
-// //General Class
-// let account = new Account(1, 'David', 0)
-// account.deposit(10)
-// console.log(account instanceof Account)
-// console.log(account.balance)
-
-// //Index signatures
-// let seats = new SeatAssignment()
-// seats.A1 = 'Kolia'
-// seats.A2 = 'David'
-// seats['Something fancy'] = 'some text'
-// console.log(seats)
-
-// //Static properties
-// let ride1 = new Ride()
-// ride1.start()
-// let ride2 = new Ride()
-// ride2.start()
-// console.log(Ride.activeRides)
-
+//General Class
+let account = new Account(1, 'David', 0)
+account.deposit(10)
+console.log(account instanceof Account)
+console.log(account.balance)
+//Index signatures
+let seats = new SeatAssignment()
+seats.A1 = 'Kolia'
+seats.A2 = 'David'
+seats['Something fancy'] = 'some text'
+console.log(seats)
+//Static properties
+let ride1 = new Ride()
+ride1.start()
+let ride2 = new Ride()
+ride2.start()
+console.log(Ride.activeRides)
 //Inheritance
-// let student = new Student(1, 'John', 'Snow')
-// console.log(student.fullName)
-// student.walk()
-// student.takeTest()
-
-// let professor = new Teacher('Kolia', 'Melkadze')
-// console.log(professor.fullName)
+let student = new Student(1, 'John', 'Snow')
+console.log(student.fullName)
+student.walk()
+student.takeTest()
+let professor = new Teacher('Kolia', 'Melkadze')
+console.log(professor.fullName)
+// Abstract classes
+let circle = new Circle(1, 'Red')
+circle.render()
