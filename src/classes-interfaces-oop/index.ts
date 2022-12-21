d9d2d4570f38a98d4b17d7e3da7613746d7c9a9f
Daviti Didbaridze <ddidbaridze@gmail.com>
2022-12-21 18:34:21 +0100

    add inheritance

--- src/classes-interfaces-oop/index.ts
+++ src/classes-interfaces-oop/index.ts
@@ -14,7 +14,6 @@ class Account {
         return this._balance
     }
 }
-
 class SeatAssignment {
     //NOTE: index signatures
     [seatNumber: string]: string
@@ -33,22 +32,49 @@ class Ride {
         return Ride._activeRides
     }
 }
-//General Class
-let account = new Account(1, 'David', 0)
-account.deposit(10)
-console.log(account instanceof Account)
-console.log(account.balance)
+class Person {
+    constructor(public firstName: string, public lastName: string) {}
 
-//Index signatures
-let seats = new SeatAssignment()
-seats.A1 = 'Kolia'
-seats.A2 = 'David'
-seats['Something fancy'] = 'some text'
-console.log(seats)
+    get fullName() {
+        return this.firstName + ' ' + this.lastName
+    }
 
-//Static properties
-let ride1 = new Ride()
-ride1.start()
-let ride2 = new Ride()
-ride2.start()
-console.log(Ride.activeRides)
+    walk() {
+        console.log('walking')
+    }
+}
+
+class Student extends Person {
+    constructor(public studentId: number, firstName: string, lastName: string) {
+        super(firstName, lastName)
+    }
+
+    takeTest() {
+        console.log('Taking a Test')
+    }
+}
+// //General Class
+// let account = new Account(1, 'David', 0)
+// account.deposit(10)
+// console.log(account instanceof Account)
+// console.log(account.balance)
+
+// //Index signatures
+// let seats = new SeatAssignment()
+// seats.A1 = 'Kolia'
+// seats.A2 = 'David'
+// seats['Something fancy'] = 'some text'
+// console.log(seats)
+
+// //Static properties
+// let ride1 = new Ride()
+// ride1.start()
+// let ride2 = new Ride()
+// ride2.start()
+// console.log(Ride.activeRides)
+
+//Inheritance
+let student = new Student(1, 'John', 'Snow')
+console.log(student.fullName)
+student.walk()
+student.takeTest()
