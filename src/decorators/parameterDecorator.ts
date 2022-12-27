26e9055771b07aa41656c9ecd810f04431535ca3
Daviti Didbaridze <ddidbaridze@gmail.com>
2022-12-27 23:09:01 +0100

    add parameter decorator

---
+++ src/decorators/parameterDecorator.ts
@@ -0,0 +1,18 @@
+type WatchedParameter = {
+    methodName: string
+    parameterIndex: number
+}
+const watchedParameters: WatchedParameter[] = []
+function Watch(target: any, methodName: string, parameterIndex: number) {
+    watchedParameters.push({
+        methodName,
+        parameterIndex
+    })
+}
+
+class Vehicle {
+    move(@Watch speed: number) {
+        console.log(speed)
+    }
+}
+console.log(watchedParameters)
