7b6f0194262538b201a1ed049b394ae185f94f39
Daviti Didbaridze <ddidbaridze@gmail.com>
2022-12-28 14:13:11 +0100

    add declaration to js

---
+++ src/utils/tax.d.ts
@@ -0,0 +1,1 @@
+export declare function calculateTaxJS(income: number): number
--- src/utils/tax.js
+++ src/utils/tax.js
@@ -1,8 +1,4 @@
-/**
- * Calculate income tax.
- * @param {number} income - Net salary after expenses
- * @returns {number}
- */
+//@ts-nocheck
 export function calculateTaxJS(income) {
     return income * 0.3
 }
